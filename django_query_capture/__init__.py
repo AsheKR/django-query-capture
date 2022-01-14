562952f7c85f6d076448ea5a7717b0bf772bde5d
Ashe <tech@ashe.kr>
2022-01-14 16:52:47 +0900

    :tada: Initial commit

---
+++ django_query_capture/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""Awesome `django-query-capture` is a Python cli/package created with https://github.com/TezRomacH/python-package-template"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
