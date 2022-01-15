6f4567095db940f263752d08d133a900a793f54d
GilYoung Song <tech@ashe.kr>
2022-01-15 13:43:17 +0900

    QueryCapture (#9)
    * ADD query_capture
    
    * ADD query_capture default Test
    
    * ADD Presenter Module Not Found Error

--- django_query_capture/__init__.py
+++ django_query_capture/__init__.py
@@ -1,7 +1,35 @@
+import typing
+
+from contextlib import ContextDecorator, ExitStack
+
+from django.utils.module_loading import import_string
+
 from .capture import native_query_capture
-from .presenter import *
+from .classify import CapturedQueryClassifier
+from .presenter import BasePresenter, RawLinePresenter
+from .utils import get_value_from_django_settings
 
 __all__ = [
+    "query_capture",
     "native_query_capture",
     "RawLinePresenter",
 ]
+
+
+class query_capture(ContextDecorator):
+    def __init__(self):
+        self._exit_stack = ExitStack().__enter__()
+        self.native_query_capture = native_query_capture()
+        self.presenter_cls: typing.Type[BasePresenter] = import_string(
+            get_value_from_django_settings("PRESENTER")
+        )
+
+    def __enter__(self):
+        self._exit_stack.enter_context(self.native_query_capture)
+        return self.native_query_capture
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.presenter_cls(
+            CapturedQueryClassifier(self.native_query_capture.captured_queries)()
+        ).print()
+        self._exit_stack.close()
--- tests/test_query_capture.py
+++ tests/test_query_capture.py
@@ -1,29 +1,16 @@
-from unittest.mock import Mock
+from django.test import TestCase, override_settings
+from news.models import Reporter
 
-from django.test import TestCase
-from django.utils import timezone
-from news.models import Article, Reporter
-
-from django_query_capture import native_query_capture
+from django_query_capture import query_capture
 
 
 class QueryCaptureTests(TestCase):
-    def setUp(self) -> None:
-        Reporter.objects.create(full_name="target")
+    def test_print(self):
+        with query_capture():
+            [Reporter.objects.create(full_name=f"target-{i}") for i in range(11)]
 
-    def test_capture_query_in_context_manager(self):
-        with native_query_capture() as q:
-            r = Reporter.objects.create(full_name="target-1")
-            Article.objects.create(
-                pub_date=timezone.now().date(),
-                headline="headline",
-                content="content",
-                reporter=r,
-            )
-            self.assertEqual(len(q), 2)
-
-    # TODO: Change to a test to see if the query is captured when using Decorator.
-    def test_capture_query_in_decorator(self):
-        func = Mock()
-        native_query_capture()(func)()
-        self.assertTrue(func.called)
+    @override_settings(PRESENTER="no.module")
+    def test_not_exist_presenter(self):
+        with self.assertRaises(ModuleNotFoundError):
+            with query_capture():
+                [Reporter.objects.create(full_name=f"target-{i}") for i in range(11)]
