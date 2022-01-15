61b160d4274164eee738118105c96f37cf52ac75
Ashe <tech@ashe.kr>
2022-01-15 19:12:08 +0900

    Modify Structure by Dependency

--- django_query_capture/__init__.py
+++ django_query_capture/__init__.py
@@ -1,14 +1,9 @@
-import typing
-
-from contextlib import ContextDecorator, ExitStack
-
-from django.utils.module_loading import import_string
-
 from .capture import native_query_capture
 from .classify import CapturedQueryClassifier
+from .decorators import query_capture
 from .middleware import QueryCaptureMiddleware
 from .presenter import BasePresenter, PrettyPresenter, RawLinePresenter
-from .utils import get_value_from_django_settings
+from .settings import get_config
 
 __all__ = [
     "BasePresenter",
@@ -18,22 +13,3 @@ __all__ = [
     "RawLinePresenter",
     "PrettyPresenter",
 ]
-
-
-class query_capture(ContextDecorator):
-    def __init__(self):
-        self._exit_stack = ExitStack().__enter__()
-        self.native_query_capture = native_query_capture()
-        self.presenter_cls: typing.Type[BasePresenter] = import_string(
-            get_value_from_django_settings("PRESENTER")
-        )
-
-    def __enter__(self):
-        self._exit_stack.enter_context(self.native_query_capture)
-        return self.native_query_capture
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.presenter_cls.print(
-            CapturedQueryClassifier(self.native_query_capture.captured_queries)()
-        )
-        self._exit_stack.close()
---
+++ django_query_capture/decorators.py
@@ -0,0 +1,29 @@
+import typing
+
+from contextlib import ContextDecorator, ExitStack
+
+from django.utils.module_loading import import_string
+
+from . import native_query_capture
+from .classify import CapturedQueryClassifier
+from .presenter import BasePresenter
+from .settings import get_config
+
+
+class query_capture(ContextDecorator):
+    def __init__(self):
+        self._exit_stack = ExitStack().__enter__()
+        self.native_query_capture = native_query_capture()
+        self.presenter_cls: typing.Type[BasePresenter] = import_string(
+            get_config()["PRESENTER"]
+        )
+
+    def __enter__(self):
+        self._exit_stack.enter_context(self.native_query_capture)
+        return self.native_query_capture
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.presenter_cls.print(
+            CapturedQueryClassifier(self.native_query_capture.captured_queries)()
+        )
+        self._exit_stack.close()
