a38216264c35ceadb169b7c0558a7a1a372bab5c
Ashe <tech@ashe.kr>
2022-01-16 15:53:34 +0900

    Confirmed the interface of BasePresenter

--- django_query_capture/presenter/base.py
+++ django_query_capture/presenter/base.py
@@ -1,12 +1,37 @@
-import re
+import typing
 
+import re
+from collections import Counter
+
+from django.utils.functional import cached_property
+
+from django_query_capture.capture import CapturedQuery
 from django_query_capture.classify import ClassifiedQuery
 from django_query_capture.settings import get_config
 
 
+class DuplicateHashableCapturedQuery(typing.Dict[str, typing.Any]):
+    def __hash__(self):
+        return hash(self["sql"])
+
+    def __eq__(self, other):
+        return self["sql"] == self["sql"]
+
+
+class SimilarHashableCapturedQuery(typing.Dict[str, typing.Any]):
+    def __hash__(self):
+        return hash(self["raw_sql"])
+
+    def __eq__(self, other):
+        return self["raw_sql"] == self["raw_sql"]
+
+
 class BasePresenter:
-    def __init__(self, classified_query: ClassifiedQuery):
+    def __init__(
+        self, classified_query: ClassifiedQuery, include_ignore_patterns: bool = False
+    ):
         self._classified_query = classified_query
+        self.include_ignore_patterns = include_ignore_patterns
 
     def print(self) -> None:
         raise NotImplementedError
@@ -19,3 +44,62 @@ class BasePresenter:
                 get_config()["IGNORE_SQL_PATTERNS"],
             )
         )
+
+    @property
+    def read_count(self) -> int:
+        return self._classified_query["read"]
+
+    @property
+    def write_count(self) -> int:
+        return self._classified_query["writes"]
+
+    @property
+    def total(self) -> int:
+        return self._classified_query["total"]
+
+    @property
+    def total_duration(self) -> float:
+        return self._classified_query["total_duration"]
+
+    @cached_property
+    def duplicates_counter(self) -> typing.Counter[CapturedQuery]:
+        counter = Counter()
+        for captured_query in self._classified_query["captured_queries"]:
+            if not self.include_ignore_patterns and not self.is_allow_pattern(
+                captured_query["sql"]
+            ):
+                continue
+            counter[DuplicateHashableCapturedQuery(captured_query)] += 1
+
+        return counter
+
+    @cached_property
+    def similar_counter(self) -> typing.Counter[CapturedQuery]:
+        counter = Counter()
+        for captured_query in self._classified_query["captured_queries"]:
+            if not self.include_ignore_patterns and not self.is_allow_pattern(
+                captured_query["sql"]
+            ):
+                continue
+
+            if (
+                self.duplicates_counter[DuplicateHashableCapturedQuery(captured_query)]
+                > get_config()["PRINT_THRESHOLDS"]["DUPLICATE_MIN_COUNT"]
+            ):
+                continue
+
+            counter[SimilarHashableCapturedQuery(captured_query)] += 1
+
+        return counter
+
+    @property
+    def most_common_duplicate(self) -> typing.Tuple[CapturedQuery, int]:
+        return self.duplicates_counter.most_common(1)[0]
+
+    @property
+    def most_common_similar(self) -> typing.Tuple[CapturedQuery, int]:
+        return self.similar_counter.most_common(1)[0]
+
+    @property
+    def captured_queries(self):
+        return self._classified_query["captured_queries"]
